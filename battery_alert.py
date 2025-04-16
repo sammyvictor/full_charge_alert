5bf7a9035dae196565c1fc71539eb2337cbf82b5
sammyvictor <sammyvic38@gmail.com>
2025-04-16 20:58:40 +0300

    Changed the method of fetching the mp3 using the os library

--- battery_alert.py
+++ battery_alert.py
@@ -1,20 +1,23 @@
-import psutil
+import os
 import time
+import psutil
 import pygame
 
 def play_alert_sound():
     pygame.mixer.init()
-    pygame.mixer.music.load("charge_complete.mp3")
+    sound_path = os.path.join(os.path.dirname(__file__), "charge_complete.mp3")
+    pygame.mixer.music.load(sound_path)
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
         time.sleep(1)
 
 def check_battery():
     battery = psutil.sensors_battery()
-    if battery and battery.percent == 100 and battery.power_plugged:
+    if battery and battery.percent >= 99 and battery.power_plugged:
         # print("Battery is full. Unplug the charger.")
         play_alert_sound()
 
-while True:    
+while True:
     check_battery()
-    time.sleep(60) 
+    time.sleep(30) 
+    
